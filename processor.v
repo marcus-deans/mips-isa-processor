d27931e1aec6979d4eef7826d4ce3472ebd19e31
Marcus Deans <57301442+marcus-deans@users.noreply.github.com>
2022-03-21 23:43:52 -0400

    Weird bug on multdiv access

--- processor.v
+++ processor.v
@@ -61,85 +61,65 @@ module processor(
 	output [31:0] data_writeReg;
 	input [31:0] data_readRegA, data_readRegB;
 
-    /* YOUR CODE STARTS HERE */
+    wire stall, branch_or_jump_taken; // Early declaration needed for stalling, branch flushing
 
-    // Create wire for stalling as well as branch flushing as needed
-    wire stall, branch_or_jump_taken;
-
-    // MODULE: PC
-    // Control unit retrieves instruction's address from memory unit
+    // PC, FD latch
     wire [31:0] pc_curr, pc_next_def, pc_next, fd_pc_out, fd_ir_out;
-    pc_reg pc(clock, reset, ~stall, pc_next, pc_curr); // Maintain count of PC
-    assign address_imem = pc_curr; // PC is used to access IMEM
-    cla_full_adder inc_pc(pc_curr, 32'b1, 1'b0, pc_curr | 32'b1, pc_curr & 32'b1, pc_next_def); // Increment the PC with ALU (just adder)
-
-    // MODULE: FETCH
-    // FD latch configured for positive clock edge while instruction memory requires a cycle to retrieve it
-    // Therefore, pc_curr will in reality latch the value of pc + 1
+    assign address_imem = pc_curr;
+    pc_reg pc(clock, reset, ~stall, pc_next, pc_curr);
+    cla_full_adder inc_pc(pc_curr, 32'b1, 1'b0, pc_curr | 32'b1, pc_curr & 32'b1, pc_next_def);
+    // since FD latch is positive clk and ins memory needs a cycle to get ins, pass in pc_curr to actually latch pc + 1
     fd_latch fd(clock, ~stall, pc_curr, branch_or_jump_taken ? 32'b0 : q_imem, fd_pc_out, fd_ir_out);
 
     // Regfile assignments
-
-    // Configure opcodes from inputs
     wire [4:0] fd_opcode;
     assign fd_opcode = fd_ir_out[31:27];
+    wire fd_is_r_type_op, fd_is_bex_op;
+    assign fd_is_r_type_op = ~fd_opcode[4] & ~fd_opcode[3] & ~fd_opcode[2] & ~fd_opcode[1] & ~fd_opcode[0];
+    assign fd_is_bex_op = fd_opcode[4] & ~fd_opcode[3] & fd_opcode[2] & fd_opcode[1] & ~fd_opcode[0];
 
-    // Determine whether type R or Bex operation
-    wire fd_of_type_r_op, fd_of_bex_op;
-    assign fd_of_type_r_op = ~fd_opcode[4] & ~fd_opcode[3] & ~fd_opcode[2] & ~fd_opcode[1] & ~fd_opcode[0];
-    assign fd_of_bex_op = fd_opcode[4] & ~fd_opcode[3] & fd_opcode[2] & fd_opcode[1] & ~fd_opcode[0];
-
-    // Register read values
     assign ctrl_readRegA = fd_ir_out[21:17];
-    assign ctrl_readRegB = fd_of_type_r_op ? fd_ir_out[16:12] : (fd_of_bex_op ? 5'd30 : fd_ir_out[26:22]);
+    assign ctrl_readRegB = fd_is_r_type_op ? fd_ir_out[16:12] : (fd_is_bex_op ? 5'd30 : fd_ir_out[26:22]);
 
-    // MODULE: DECODE 
-    // Setup DX latch
+    // DX latch
     wire [31:0] dx_ir_in, dx_pc_out, dx_a_out, dx_b_out, dx_ir_out;
     assign dx_ir_in = stall ? 32'b0 : fd_ir_out;
     dx_latch dx(clock, fd_pc_out, data_readRegA, data_readRegB, branch_or_jump_taken ? 32'b0 : dx_ir_in, dx_pc_out, dx_a_out, dx_b_out, dx_ir_out);
 
-    // Opcode for operation
-    wire [4:0] dx_opcode;
-    assign dx_opcode = dx_ir_out[31:27];
-
-    // MODULE: EXECUTE
-    // For bypassing, declare select wires here
-    wire [1:0] mux_a_select, mux_b_select;
-    wire [31:0] xm_o_out;
-    
-    // ALU instantiation and configuration
-    wire [31:0] alu_in_a, alu_in_b;
-    mux_4 alu_a_mux(alu_in_a, mux_a_select, xm_o_out, data_writeReg, dx_a_out, 32'b0);
-    wire [31:0] alu_b_mux_out;
-    mux_4 alu_b_mux(alu_b_mux_out, mux_b_select, xm_o_out, data_writeReg, dx_b_out, 32'b0);
-
-    // Configure immediate value for ALU
+    // ALU
     wire [31:0] sx_imm;
     assign sx_imm[16:0] = dx_ir_out[16:0];
     assign sx_imm[31:17] = dx_ir_out[16] ? 15'b111111111111111 : 15'b0;
 
-    // Determine whether operation branches or is of type R and configure ALU input accordingly
-    wire dx_of_branch_op, dx_of_type_r_op;
-    assign dx_of_type_r_op = ~dx_opcode[4] & ~dx_opcode[3] & ~dx_opcode[2] & ~dx_opcode[1] & ~dx_opcode[0];
-    assign dx_of_branch_op = (~dx_opcode[4] & ~dx_opcode[3] & ~dx_opcode[2] & dx_opcode[1] & ~dx_opcode[0]) | 
+    wire [4:0] dx_opcode;
+    assign dx_opcode = dx_ir_out[31:27];
+    wire [31:0] alu_in_a, alu_in_b;
+    
+    // Early declarations of these wires needed for bypassing
+    wire [1:0] mux_a_select, mux_b_select;
+    wire [31:0] xm_o_out;
+    
+    mux_4 alu_a_mux(alu_in_a, mux_a_select, xm_o_out, data_writeReg, dx_a_out, 32'b0);
+    wire [31:0] alu_b_mux_out;
+    mux_4 alu_b_mux(alu_b_mux_out, mux_b_select, xm_o_out, data_writeReg, dx_b_out, 32'b0);
+
+    wire dx_is_r_type_op, dx_is_branch_op;
+    assign dx_is_r_type_op = ~dx_opcode[4] & ~dx_opcode[3] & ~dx_opcode[2] & ~dx_opcode[1] & ~dx_opcode[0];
+    assign dx_is_branch_op = (~dx_opcode[4] & ~dx_opcode[3] & ~dx_opcode[2] & dx_opcode[1] & ~dx_opcode[0]) | 
         (~dx_opcode[4] & ~dx_opcode[3] & dx_opcode[2] & dx_opcode[1] & ~dx_opcode[0]);
+    assign alu_in_b = (dx_is_r_type_op || dx_is_branch_op) ? alu_b_mux_out : sx_imm;
 
-    // Configure ALU opcode, inputs,and shift amount
-    wire [4:0] alu_opcode, shift_amount;
-    assign alu_opcode = dx_of_type_r_op ? dx_ir_out[6:2] : (dx_of_branch_op ? 5'b1 : 5'b0);
-    assign shift_amount = dx_of_type_r_op ? dx_ir_out[11:7] : 5'b0;
-    assign alu_in_b = (dx_of_type_r_op || dx_of_branch_op) ? alu_b_mux_out : sx_imm;
-
-    // Outputs of ALU and ALU unit itself
+    wire [4:0] alu_opcode, shamt;
+    assign alu_opcode = dx_is_r_type_op ? dx_ir_out[6:2] : (dx_is_branch_op ? 5'b1 : 5'b0);
+    assign shamt = dx_is_r_type_op ? dx_ir_out[11:7] : 5'b0;
     wire [31:0] alu_out, alu_out_ovf;
     wire is_not_equal, is_less_than, alu_overflow;
-    alu alu_unit(alu_in_a, alu_in_b, alu_opcode, shift_amount, alu_out, is_not_equal, is_less_than, alu_overflow);
+    alu alu_unit(alu_in_a, alu_in_b, alu_opcode, shamt, alu_out, is_not_equal, is_less_than, alu_overflow);
 
-    // MultDiv operation
+    // MultDiv
     wire ctrl_mult, ctrl_div;
-    assign ctrl_mult = dx_of_type_r_op & ~alu_opcode[4] & ~alu_opcode[3] & alu_opcode[2] & alu_opcode[1] & ~alu_opcode[0];
-    assign ctrl_div = dx_of_type_r_op & ~alu_opcode[4] & ~alu_opcode[3] & alu_opcode[2] & alu_opcode[1] & alu_opcode[0];
+    assign ctrl_mult = dx_is_r_type_op & ~alu_opcode[4] & ~alu_opcode[3] & alu_opcode[2] & alu_opcode[1] & ~alu_opcode[0];
+    assign ctrl_div = dx_is_r_type_op & ~alu_opcode[4] & ~alu_opcode[3] & alu_opcode[2] & alu_opcode[1] & alu_opcode[0];
 
     wire [31:0] multdiv_in_a, multdiv_in_b, multdiv_ir, multdiv_result;
     wire multdiv_is_running, multdiv_exception, multdiv_result_ready;
@@ -200,22 +180,22 @@ module processor(
     assign mw_is_jal_op = ~mw_opcode[4] & ~mw_opcode[3] & ~mw_opcode[2] & mw_opcode[1] & mw_opcode[0];
     assign mw_is_setx_op = mw_opcode[4] & ~mw_opcode[3] & mw_opcode[2] & ~mw_opcode[1] & mw_opcode[0];
 
-    // Create multiple tri state buffers for ctrl_writeReg
-    tri_buffer_5 tri_jal_reg(5'd31, !(mw_ovf_out || mw_is_setx_op) && mw_is_jal_op && !(multdiv_result_ready && !multdiv_exception), ctrl_writeReg);
+    // tri buffers for ctrl_writeReg
     tri_buffer_5 tri_normal_reg(mw_ir_out[26:22], !(mw_ovf_out || mw_is_setx_op) && !mw_is_jal_op && !(multdiv_result_ready && !multdiv_exception), ctrl_writeReg);
-    tri_buffer_5 tri_multdiv_reg(multdiv_ir[26:22], !(mw_ovf_out || mw_is_setx_op) && !mw_is_jal_op && (multdiv_result_ready && !multdiv_exception), ctrl_writeReg);
     tri_buffer_5 tri_status_reg(5'd30, (mw_ovf_out || mw_is_setx_op) && !mw_is_jal_op && !(multdiv_result_ready && !multdiv_exception), ctrl_writeReg);
+    tri_buffer_5 tri_jal_reg(5'd31, !(mw_ovf_out || mw_is_setx_op) && mw_is_jal_op && !(multdiv_result_ready && !multdiv_exception), ctrl_writeReg);
+    tri_buffer_5 tri_multdiv_reg(multdiv_ir[26:22], !(mw_ovf_out || mw_is_setx_op) && !mw_is_jal_op && (multdiv_result_ready && !multdiv_exception), ctrl_writeReg);
 
-    // Write the data to the relevant registers
     assign data_writeReg = mw_is_lw_op ? mw_d_out : ((multdiv_result_ready && !multdiv_exception) ? multdiv_result : mw_o_out);
     assign ctrl_writeEnable = mw_is_r_type_op | mw_is_addi_op | mw_is_lw_op | mw_is_jal_op | mw_is_setx_op | (multdiv_result_ready && !multdiv_exception);
 
-    // Control logic
-    control control_unit(pc_next_def, dx_pc_out, sx_imm, alu_b_mux_out, dx_ir_out, is_not_equal, is_not_equal ? ~is_less_than : 1'b0, pc_next, branch_or_jump_taken);
+    // Bypass logic
+    bypass bypass_unit(dx_ir_out, xm_ir_out, mw_ir_out, xm_ovf_out, mw_ovf_out, mux_a_select, mux_b_select, wm_bypass);
 
     // Stall logic
     stall stall_unit(fd_ir_out, dx_ir_out, xm_ir_out, multdiv_is_running, multdiv_result_ready, stall);
 
-    // Bypass logic
-    bypass bypass_unit(dx_ir_out, xm_ir_out, mw_ir_out, xm_ovf_out, mw_ovf_out, mux_a_select, mux_b_select, wm_bypass);
+    // Control logic
+    control control_unit(pc_next_def, dx_pc_out, sx_imm, alu_b_mux_out, dx_ir_out, is_not_equal, is_not_equal ? ~is_less_than : 1'b0, pc_next, branch_or_jump_taken);
+
 endmodule
