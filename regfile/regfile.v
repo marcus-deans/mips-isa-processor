96637ca86772e3428c3936c48649f25faa067bb7
Marcus Deans <57301442+marcus-deans@users.noreply.github.com>
2022-03-21 22:35:14 -0400

    Decoder syntax again

--- regfile/regfile.v
+++ regfile/regfile.v
@@ -66,7 +66,7 @@ module regfile (
 
 	// decoder
 	wire [31:0] writeEnable;
-	decoder_32 decoder(ctrl_writeReg, ctrl_writeEnable, writeEnable);
+	decoder32 decoder(ctrl_writeReg, ctrl_writeEnable, writeEnable);
 
 	// create 32 registers, register 0 always has an output of 0 (disable writing)
 	wire [31:0] out0, out1, out2, out3, out4, out5, out6, out7, out8, out9, out10, out11, 
